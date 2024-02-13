45d7b7cd193e4dd57f857421e239e561a290db6a
ONUR ALP AKIN <onur.alp.akin@proton.me>
2024-02-13 22:01:23 +0300

    update script

--- configure.py
+++ configure.py
@@ -4,68 +4,36 @@ import yaml
 import subprocess
 import argparse
 
-# ------------------------------
-# ----------dot-access----------
-# ------------------------------
-class edict(dict):
-    # https://gist.github.com/markhu/fbbab71359af00e527d0?permalink_comment_id=3189115#gistcomment-3189115
-    
-    __setattr__= dict.__setitem__
-    __delattr__= dict.__delitem__
-
-    def __init__(self, data):
-        if isinstance(data, str):
-            data = json.loads(data)
-    
-        for name, value in data.items():
-            setattr(self, name, self._wrap(value))
-
-    def __getattr__(self, attr):
-        def _traverse(obj, attr):
-            if self._is_indexable(obj):
-                try:
-                    return obj[int(attr)]
-                except:
-                    return None
-            elif isinstance(obj, dict):
-                return obj.get(attr, None)
-            else:
-                return attr
-        if '.' in attr:
-            return reduce(_traverse, attr.split('.'), self)
-        return self.get(attr, None)
-
-    def _wrap(self, value):
-        if self._is_indexable(value):
-            # (!) recursive (!)
-            return type(value)([self._wrap(v) for v in value])
-        elif isinstance(value, dict):
-            return edict(value)
-        else:
-            return value
-    
-    @staticmethod
-    def _is_indexable(obj):
-        return isinstance(obj, (tuple, list, set, frozenset))
-
 # ------------------------------------------
 # --------------argument-parse--------------
 # ------------------------------------------
 parser = argparse.ArgumentParser(description='Configure the system')
 parser.add_argument("--show",
                     help="Show what can you execute",
-                    metavar="",
-                    action=argparse.BooleanOptionalAction,
-                    type=bool)
+                    action='store_true'
+                    )
 parser.add_argument("--command",
                     help="Command to execute from list",
-                    type=str)
+                    )
 
 # ---------------------------------------
 # -------------actual-script-------------
 # ---------------------------------------
-# def show_commands(bst):
 
+def print_dict_keys(dictionary, indent=0):
+    for key, value in dictionary.items():
+        print(" " * indent, key)
+        if (isinstance(value, dict)):
+            print_dict_keys(value, indent+4)
+
+def get_dict_key(dictionary, keys):
+    assert dictionary is not None, "key error. check if command exists"
+    if "." in keys:
+        key, rest = keys.split(".", 1)
+        print()
+        return get_dict_key(dictionary.get(key), rest)
+    else:
+        return dictionary[keys]
 
 def run_command(command):
     sp = subprocess.run(command.split(), stdout=subprocess.PIPE)
@@ -75,19 +43,24 @@ def main(bst):
     args = parser.parse_args()
 
     if (args.show):
-        print('showing arguments')
+        print_dict_keys(bst.get("command"))
     elif (args.command):
-        print("executing command", args.command)
+        command = get_dict_key(bst.get("command"), args.command)
+        assert isinstance(command, str), "command must not be parent, incorrect usage"
+
+        action = get_dict_key(bst.get("action"), args.command)
+        assert isinstance(action, list), "command actions must be a list, fix yaml file"
+
+        for act in action:
+            run_command("{} {}".format(command, act))
     else:
         parser.print_help()
-
-    # run_command(bst.commands.list_packages.flatpak)
 
 # ------------------------------------------
 # --------------load-yaml-file--------------
 # ------------------------------------------
 with open("bootstrap.yml", "r") as stream:
     try:
-        main(edict(yaml.safe_load(stream)))
+        main(yaml.safe_load(stream))
     except yaml.YAMLError as exc:
         print(exc)

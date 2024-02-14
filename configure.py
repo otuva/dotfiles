b4166ea77f1d8b4de7a60742b44144207455b0ea
ONUR ALP AKIN <onur.alp.akin@proton.me>
2024-02-14 12:37:07 +0300

    update py script

--- configure.py
+++ configure.py
@@ -8,13 +8,18 @@ import argparse
 # --------------argument-parse--------------
 # ------------------------------------------
 parser = argparse.ArgumentParser(description='Configure the system')
-parser.add_argument("--show",
-                    help="Show what can you execute",
+parser.add_argument("--list",
+                    help="List every command you can execute",
                     action='store_true'
                     )
 parser.add_argument("--command",
-                    help="Command to execute from list",
+                    metavar="path",
+                    help="Execute command",
                     )
+parser.add_argument("--show",
+                    metavar="path",
+                    help="Show which command would be executed",
+                    )         
 
 # ---------------------------------------
 # -------------actual-script-------------
@@ -30,20 +35,29 @@ def get_dict_key(dictionary, keys):
     assert dictionary is not None, "key error. check if command exists"
     if "." in keys:
         key, rest = keys.split(".", 1)
-        print()
         return get_dict_key(dictionary.get(key), rest)
     else:
         return dictionary[keys]
 
 def run_command(command):
     sp = subprocess.run(command.split(), stdout=subprocess.PIPE)
-    print(sp.stdout)
+    if (sp.stdout):
+        print(sp.stdout)
 
 def main(bst):
     args = parser.parse_args()
 
-    if (args.show):
+    if (args.list):
         print_dict_keys(bst.get("command"))
+    elif (args.show):
+        command = get_dict_key(bst.get("command"), args.show)
+        assert isinstance(command, str), "command must not be parent, incorrect usage"
+
+        action = get_dict_key(bst.get("action"), args.show)
+        assert isinstance(action, list), "command actions must be a list, fix yaml file"
+        
+        for act in action:
+            print(f"{command} {act}")
     elif (args.command):
         command = get_dict_key(bst.get("command"), args.command)
         assert isinstance(command, str), "command must not be parent, incorrect usage"
@@ -52,7 +66,7 @@ def main(bst):
         assert isinstance(action, list), "command actions must be a list, fix yaml file"
 
         for act in action:
-            run_command("{} {}".format(command, act))
+            run_command(f"{command} {act}")
     else:
         parser.print_help()
 
